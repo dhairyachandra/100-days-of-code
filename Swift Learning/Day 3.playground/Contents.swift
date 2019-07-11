4e5d5cbaf2051846f21cb628746b9403331d5cc2
Dhairya Chandra <dhairyachandra@outlook.com>
2019-07-12 00:16:48 +0530

    Day 3 - Loops
    Done with loops in swift

---
+++ Swift Learning/Day 3.playground/Contents.swift
@@ -0,0 +1,44 @@
+// Developer: Dhairya Chandra
+
+// DAY 3: 100 Days Of Code
+// Today's Progress: Learned about loops (For in, while, repeat-while)
+
+// Profile: www.meDhairya.com
+
+// --------------------
+//        LOOPS
+// --------------------
+
+import UIKit
+
+var str = "Day 3, Loops"
+
+for _ in 1...5 {
+    print("meDhairya")
+}
+
+var sum = 0
+// Code for sum of first 5 numbers
+for index in 1...5 {
+    sum += index
+}
+print (sum)
+
+// While loop
+var count = 3
+while count > 0 {
+    print ("While Loop")
+    count -= 1
+}
+
+// Repeat while loop
+var rpt = 3
+repeat {
+        print("repeat while loop")
+        rpt -= 1
+} while rpt > 0
+
+
+
+
+
--- log.md
+++ log.md
@@ -1,8 +1,14 @@
 # 100 Days Of Code - Log
 
 
+### Day 3: July 11, Thursday 
 
-### Day 2: July 8, Sunday
+**Today's Progress**: I have learned about loops (For-in loops, while loops and repeat while loops)
+
+**Link of work**:  [Day 3 - loops statements](https://github.com/dhairyachandra/100-days-of-code/blob/master/Swift%20Learning/Day%203.playground/Contents.swift)
+
+
+### Day 2: July 8, Monday
 
 **Today's Progress**: learned about getting started with iOS development, if and switch statement in swift.
 
